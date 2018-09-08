8da460ea6e37a66a6c13e1672a9df19b2d3d2aea
Shion Yamashita <shioyama1118@gmail.com>
2018-09-08 15:10:02 +0900

    Added partition

---
+++ alds1_6_b_partition/target/debug/deps/alds1_6_b_partition-ef41e4028acded52.d
@@ -0,0 +1,5 @@
+/home/shion/aoj_rust/alds1_6_b_partition/target/debug/deps/alds1_6_b_partition-ef41e4028acded52.rmeta: src/main.rs
+
+/home/shion/aoj_rust/alds1_6_b_partition/target/debug/deps/alds1_6_b_partition-ef41e4028acded52.d: src/main.rs
+
+src/main.rs:
---
+++ alds1_6_b_partition/target/debug/libalds1_6_b_partition.d
@@ -0,0 +1,1 @@
+/home/shion/aoj_rust/alds1_6_b_partition/target/debug/libalds1_6_b_partition.rmeta: /home/shion/aoj_rust/alds1_6_b_partition/src/main.rs
