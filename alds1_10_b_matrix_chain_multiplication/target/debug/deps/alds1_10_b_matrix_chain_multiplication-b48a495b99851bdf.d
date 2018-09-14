95c9931fc3cd9e12c4a38d0cf826d5d857f7cd76
Shion Yamashita <shioyama1118@gmail.com>
2018-09-14 09:40:52 +0900

    Added matric chain multiplication problem

---
+++ alds1_10_b_matrix_chain_multiplication/target/debug/deps/alds1_10_b_matrix_chain_multiplication-b48a495b99851bdf.d
@@ -0,0 +1,5 @@
+/home/shion/aoj_rust/alds1_10_b_matrix_chain_multiplication/target/debug/deps/alds1_10_b_matrix_chain_multiplication-b48a495b99851bdf.rmeta: src/main.rs
+
+/home/shion/aoj_rust/alds1_10_b_matrix_chain_multiplication/target/debug/deps/alds1_10_b_matrix_chain_multiplication-b48a495b99851bdf.d: src/main.rs
+
+src/main.rs:
