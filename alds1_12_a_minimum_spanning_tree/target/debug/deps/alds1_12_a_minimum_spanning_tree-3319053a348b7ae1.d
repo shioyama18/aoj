4bcc23bdf817be7136a5cdb4b3afb9cb8c3b2836
Shion Yamashita <shioyama1118@gmail.com>
2018-09-17 22:02:20 +0900

    Added minimum spanning tree

---
+++ alds1_12_a_minimum_spanning_tree/target/debug/deps/alds1_12_a_minimum_spanning_tree-3319053a348b7ae1.d
@@ -0,0 +1,5 @@
+/home/shion/aoj_rust/alds1_12_a_minimum_spanning_tree/target/debug/deps/alds1_12_a_minimum_spanning_tree-3319053a348b7ae1: src/main.rs
+
+/home/shion/aoj_rust/alds1_12_a_minimum_spanning_tree/target/debug/deps/alds1_12_a_minimum_spanning_tree-3319053a348b7ae1.d: src/main.rs
+
+src/main.rs:
