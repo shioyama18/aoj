997cbed0bc9bd91b5f8e4142c35ebf6d4e246bc5
Shion Yamashita <shioyama1118@gmail.com>
2019-01-24 15:25:58 +0900

    Updated maximum profit

--- alds1_01_d_maximum_profit/src/main.rs
+++ alds1_01_d_maximum_profit/src/main.rs
@@ -1,23 +1,58 @@
 // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_D
 
-use std::io;
-use std::cmp::{min, max};
-
-fn read() -> isize {
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).unwrap();
-    n.trim().parse::<isize>().unwrap()
+macro_rules! scan {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| scan!($t ;;)).collect::<Vec<_>>()
+    };
 }
 
+use std::cmp::{max, min};
+
 fn main() {
-    let n = read();
-    let r0 = read();
-    let r1 = read();
-    let mut minv: isize = min(r0, r1);
-    let mut maxv: isize = r1 - r0;
+    let n = scan!(usize);
+    let r0 = scan!(isize);
+    let r1 = scan!(isize);
+    let mut minv = min(r0, r1);
+    let mut maxv = r1 - r0;
 
     for _ in 2..n {
-        let r = read();
+        let r = scan!(isize);
         maxv = max(maxv, r - minv);
         minv = min(minv, r);
     }
