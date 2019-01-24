9e23013800f09973011b1d18de9a05877c0352b6
Shion Yamashita <shioyama1118@gmail.com>
2019-01-24 15:07:39 +0900

    Updated primes

--- alds1_01_c_prime_numbers/src/main.rs
+++ alds1_01_c_prime_numbers/src/main.rs
@@ -42,37 +42,32 @@ macro_rules! scan {
     };
 }
 
-fn main() -> Result<(), Box<std::error::Error>> {
+fn main() {
     let n = scan!(usize);
 
     let mut counter = 0;
-    let mut max = 2;
+    let mut i = 2;
     let mut primes = Vec::new();
 
     for _ in 0..n {
-        // Read number
         let x = scan!(usize);
 
-        // Add all prime numbers upto and including x to list of primes
-        'outer: while max <= x {
-            for p in primes.iter() {
-                if max % p == 0 {
-                    max += 1;
-                    continue 'outer;
-                }
+        while i * i <= x {
+            if primes.clone().iter().all(|p| i % p != 0) {
+                primes.push(i);
             }
-
-            primes.push(max);
-            max += 1;
+            i += 1;
         }
 
-        // If x is in primes, increment the counter
         if primes.contains(&x) {
             counter += 1;
+        } else {
+            if primes.clone().iter().all(|p| x % p != 0) {
+                primes.push(x);
+                counter += 1;
+            }
         }
     }
 
     println!("{}", counter);
-
-    Ok(())
 }
