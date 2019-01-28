3b43661c55fdbe6428454d6f1da3dcd57a2701ac
Shion Yamashita <shioyama1118@gmail.com>
2019-01-28 12:02:17 +0900

    Updated binary search

---
+++ alds1_04_b_binary_search/src/main.rs
@@ -0,0 +1,76 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_B
+
+macro_rules! scan {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+                    scan!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| scan!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+use std::cmp::Ordering;
+
+fn binary_search(s: &[usize], i: usize) -> Option<usize> {
+    if s.is_empty() {
+        return None;
+    }
+
+    let n = s.len();
+    let mid_index = n / 2;
+    let mid = s[mid_index];
+
+    match mid.cmp(&i) {
+        Ordering::Less => binary_search(&s[(mid_index + 1)..n], i),
+        Ordering::Equal => Some(mid),
+        Ordering::Greater => binary_search(&s[0..mid_index], i),
+    }
+}
+
+fn main() {
+    let _n = scan!(usize);
+    let s = scan!(usize ;;);
+    let _q = scan!(usize);
+    let t = scan!(usize ;;);
+
+    let mut c: usize = 0;
+    for i in t {
+        if let Some(_) = binary_search(&s, i) {
+            c += 1;
+        }
+    }
+    println!("{}", c);
+}
